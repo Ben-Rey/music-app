c4d8c55fe1b402f946db2e0b3400822ab9ace12a
Ben Rey <benjamin@morio.co>
2021-04-18 20:37:44 +0200

    separate repo back front

--- back/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const authRouter = require("express").Router({ mergeParams: true });
-const AuthController = require("../controllers/auth.controller");
-
-authRouter.post("/login", new AuthController().login);
-authRouter.post("/register", new AuthController().register);
-authRouter.post("/checkToken", new AuthController().checkToken);
-
-module.exports = authRouter;
--- back/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const usersRouter = require("express").Router({ mergeParams: true });
-const userController = require("../controllers/users.controller");
-
-usersRouter.post("/", new userController().create);
-usersRouter.get("/", new userController().list);
-usersRouter.get("/:userId", new userController().get);
-
-module.exports = usersRouter;
